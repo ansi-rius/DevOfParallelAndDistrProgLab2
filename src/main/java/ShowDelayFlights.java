09e6e3ce2bd7e189940b8f03e3140f768c6a109a
Anemone <tttaina@mail.ru>
2019-10-24 19:58:31 +0300

    Scripted auto-commit on change (2019-10-24 19:58:31) by gitwatch.sh

--- src/main/java/ShowDelayFlights.java
+++ src/main/java/ShowDelayFlights.java
@@ -19,6 +19,9 @@ public class ShowDelayFlights {
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportsTableMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         //партишнер в)
+        //компаратор г)
+        //редьюс-функц д)
         //
+        
     }
 }
