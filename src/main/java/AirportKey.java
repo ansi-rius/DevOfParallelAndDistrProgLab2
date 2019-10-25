9c1291442a3039a5f7ea0e72b3288ab3e51a5b1a
Anemone <tttaina@mail.ru>
2019-10-25 23:45:33 +0300

    Scripted auto-commit on change (2019-10-25 23:45:33) by gitwatch.sh

--- src/main/java/AirportKey.java
+++ src/main/java/AirportKey.java
@@ -1,4 +1,5 @@
-//в) 
+//в)  в.разрабатываем map функцию для каждого из набора данных, которая
+//генерирует WritableComparable ключа и Writable данных
 import org.apache.hadoop.io.WritableComparable;
 
 public class AirportKey implements WritableComparable {
