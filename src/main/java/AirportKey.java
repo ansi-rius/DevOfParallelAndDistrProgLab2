e0101f67dc16a29a71d6084e71d267502897eb38
Anemone <tttaina@mail.ru>
2019-10-25 23:50:23 +0300

    Scripted auto-commit on change (2019-10-25 23:50:23) by gitwatch.sh

--- src/main/java/AirportKey.java
+++ src/main/java/AirportKey.java
@@ -4,7 +4,12 @@
 //для списка рейсов в качестве value эта функция отправляет время задержки (в
 //виде строки)
 import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.Text;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 
 public class AirportKey implements WritableComparable<AirportKey> {
-    
+    private Text airportName;
+    private int 
 }
