b2981f33ef3774df4935b7e7cb719e3d4e8cd6c9
Anemone <tttaina@mail.ru>
2019-10-26 02:16:13 +0300

    Scripted auto-commit on change (2019-10-26 02:16:13) by gitwatch.sh

--- src/main/java/sorryIdontKnowHowToNameIt/AirportReducer.java
+++ src/main/java/sorryIdontKnowHowToNameIt/AirportReducer.java
@@ -1,4 +1,29 @@
 package sorryIdontKnowHowToNameIt;
 
-public class AirportReducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class AirportReducer extends Reducer<AirportKey, Text, Text, Text> {
+    public void reduce(AirportKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        int count = 0;
+        double maxTime=Double.MIN_VALUE, minTime = Double.MAX_VALUE, time, averageTime=0;
+        Iterator<Text> iter = values.iterator();
+        Text airportName = new Text(iter.next());
+        while(iter.hasNext()) {
+            averageTime = Double.parseDouble(iter.next().toString());
+            time = averageTime;
+            maxTime = Math.max(maxTime, time);
+            minTime = Math.min(minTime, time);
+            count++;
+            averageTime += time;
+        }
+
+        if (count == 0)
+            return;
+        averageTime=averageTime/count;
+        context.write(new Text(airportName), new Text("MIN time = "+minTime+" MAX time = "+maxTime+"Average time = "+averageTime));
+    }
 }
