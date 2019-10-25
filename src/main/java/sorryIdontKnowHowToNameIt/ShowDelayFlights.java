63e1e0ecf7e575b42256559d8d41bde1ed8cec8b
Anemone <tttaina@mail.ru>
2019-10-26 01:23:16 +0300

    Scripted auto-commit on change (2019-10-26 01:23:16) by gitwatch.sh

--- src/main/java/sorryIdontKnowHowToNameIt/ShowDelayFlights.java
+++ src/main/java/sorryIdontKnowHowToNameIt/ShowDelayFlights.java
@@ -18,8 +18,8 @@ public class ShowDelayFlights {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightsTableMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportsTableMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        //партишнер в)
-        //компаратор г)
+        job.setPartitionerClass(AirportPartitioner.class); //партишнер в)
+        job.setGroupingComparatorClass();//компаратор г)
         //редьюс-функц д)
         //
         job.setOutputKeyClass(Text.class);
