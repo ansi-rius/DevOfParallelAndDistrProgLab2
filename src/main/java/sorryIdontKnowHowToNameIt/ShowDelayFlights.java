2f7b93ca6605165704be1a660dfc23613d74800f
Anemone <tttaina@mail.ru>
2019-10-26 01:58:38 +0300

    Scripted auto-commit on change (2019-10-26 01:58:38) by gitwatch.sh

--- src/main/java/sorryIdontKnowHowToNameIt/ShowDelayFlights.java
+++ src/main/java/sorryIdontKnowHowToNameIt/ShowDelayFlights.java
@@ -20,7 +20,7 @@ public class ShowDelayFlights {
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setPartitionerClass(AirportPartitioner.class); //партишнер в)
         job.setGroupingComparatorClass(KeyComparator.class);//компаратор г)
-        job.setReducerClass(AirportRe)//редьюс-функц д)
+        job.setReducerClass(AirportReducer.class)//редьюс-функц д)
         //
         job.setOutputKeyClass(Text.class);
         //job.setOutputValueClass(IntWritable.class); //врайтбл менять
