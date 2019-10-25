2ebfbe34f52eaf4ae5758738f3772519fe1bb269
Anemone <tttaina@mail.ru>
2019-10-26 01:22:10 +0300

    Scripted auto-commit on change (2019-10-26 01:22:10) by gitwatch.sh

--- src/main/java/sorryIdontKnowHowToNameIt/AirportPartitioner.java
+++ src/main/java/sorryIdontKnowHowToNameIt/AirportPartitioner.java
@@ -1,4 +1,9 @@
 package sorryIdontKnowHowToNameIt;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
 
-public class AirportPartitioner {
+public class AirportPartitioner extends Partitioner<AirportKey, Text> {
+    public int getPartition(AirportKey key, Text value, int numReduceTasks) {
+        return (key.getAirportName().hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+    }
 }
