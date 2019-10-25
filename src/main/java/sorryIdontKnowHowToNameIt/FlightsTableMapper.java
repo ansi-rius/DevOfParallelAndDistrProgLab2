00aa690a078f9b0c28871ce404d11da485fdcb05
Anemone <tttaina@mail.ru>
2019-10-26 01:01:38 +0300

    Scripted auto-commit on change (2019-10-26 01:01:38) by gitwatch.sh

--- src/main/java/sorryIdontKnowHowToNameIt/FlightsTableMapper.java
+++ src/main/java/sorryIdontKnowHowToNameIt/FlightsTableMapper.java
@@ -1,5 +1,17 @@
 package sorryIdontKnowHowToNameIt;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 
-public class FlightsTableMapper {
+import java.io.IOException;
 
+public class FlightsTableMapper extends Mapper<LongWritable, Text, AirportKey, Text> {
+    private static int CODE = 14, DELAY = 17;
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if (key.get() == 0) {
+            return;
+        }
+        String codesAndAirports[] = TablesParser.parseAirportsTable(value); //результат парсинга таблицы
+        
+    }
 }
