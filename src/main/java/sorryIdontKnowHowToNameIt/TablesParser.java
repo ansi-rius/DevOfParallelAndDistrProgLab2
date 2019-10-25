f382e4e0d8bf6f7b7926196bacc7838d439b1282
Anemone <tttaina@mail.ru>
2019-10-26 00:23:21 +0300

    Scripted auto-commit on change (2019-10-26 00:23:21) by gitwatch.sh

--- src/main/java/sorryIdontKnowHowToNameIt/TablesParser.java
+++ src/main/java/sorryIdontKnowHowToNameIt/TablesParser.java
@@ -14,4 +14,6 @@ public class TablesParser {
         //2015,1,1,10,6,2015-01-10,"AA",19805,"AA","N790AA","1",12478,1247802,31703,12892,"1225","1235",0.00,0.00,0.00,"",345.00,2475.00,
         return text.toString().split(","); // a[14]=code, a[17] = delay
     }
+
+    public static int get
 }
