aa1344b0482f05c2898236f6f44fdfb62e074939
Anemone <tttaina@mail.ru>
2019-10-25 23:46:22 +0300

    Scripted auto-commit on change (2019-10-25 23:46:22) by gitwatch.sh

--- src/main/java/AirportKey.java
+++ src/main/java/AirportKey.java
@@ -1,6 +1,10 @@
 //в)  в.разрабатываем map функцию для каждого из набора данных, которая
 //генерирует WritableComparable ключа и Writable данных
+// для списка аэропортов эта функция в качестве value отправляет имя аэропорта.
+//для списка рейсов в качестве value эта функция отправляет время задержки (в
+//виде строки)
 import org.apache.hadoop.io.WritableComparable;
 
-public class AirportKey implements WritableComparable {
+public class AirportKey implements WritableComparable<AirportKey> {
+    
 }
