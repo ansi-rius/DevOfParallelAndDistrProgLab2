ec03ed7ef5b5fdc7fc9188a6d498f9bdf3ed24db
Anemone <tttaina@mail.ru>
2019-10-25 23:32:06 +0300

    Scripted auto-commit on change (2019-10-25 23:32:06) by gitwatch.sh

--- src/main/java/TablesParser.java
+++ src/main/java/TablesParser.java
@@ -9,6 +9,7 @@ public class TablesParser {
     }
 
     public static String[] parseFlightsTable(Text text) {
+        //2015,1,1,10,6,2015-01-10,"AA",19805,"AA","N790AA","1",12478,1247802,31703,12892,"1225","1235",0.00,0.00,0.00,"",345.00,2475.00,
         return text.toString().split(",");
     }
 }
