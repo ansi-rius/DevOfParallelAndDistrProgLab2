d50c1893aa827199a715c1016897ef34f568a51c
Anemone <tttaina@mail.ru>
2019-10-26 01:28:23 +0300

    Scripted auto-commit on change (2019-10-26 01:28:23) by gitwatch.sh

--- src/main/java/sorryIdontKnowHowToNameIt/KeyComparator.java
+++ src/main/java/sorryIdontKnowHowToNameIt/KeyComparator.java
@@ -1,5 +1,17 @@
 package sorryIdontKnowHowToNameIt;
 
-public class KeyComparator {
-    
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+
+public class KeyComparator extends WritableComparator{
+    protected KeyComparator() {
+        super(AirportKey.class, true);
+    }
+
+    public int compare(WritableComparable a, WritableComparable b) {
+        AirportKey a1 = (AirportKey)a;
+        AirportKey b1 = (AirportKey)b;
+        return a1.getAirportName().
+    }
 }
