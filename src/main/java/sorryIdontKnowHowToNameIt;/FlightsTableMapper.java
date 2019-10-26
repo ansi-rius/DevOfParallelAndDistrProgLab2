eb5e7b98597620dcbd86127d8367dcf877093c72
Anemone <tttaina@mail.ru>
2019-10-26 03:19:01 +0300

    Scripted auto-commit on change (2019-10-26 03:19:01) by gitwatch.sh

--- src/main/java/sorryIdontKnowHowToNameIt;/FlightsTableMapper.java
+++ src/main/java/sorryIdontKnowHowToNameIt;/FlightsTableMapper.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class FlightsTableMapper extends Mapper<LongWritable, Text, sorryIdontKnowHowToNameIt.AirportKey, Text> {
     private static int CODE = 14, DELAY = 17;
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String codesAndAirports[] = sorryIdontKnowHowToNameIt.TablesParser.parseAirportsTable(value); //результат парсинга таблицы
+        String codesAndAirports[] = sorryIdontKnowHowToNameIt.TablesParser.parseFlightsTable(value); //результат парсинга таблицы
         if (sorryIdontKnowHowToNameIt.TablesParser.checkFlight(codesAndAirports[DELAY])) {
             return;
         }
