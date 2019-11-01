e739d8347140d163fb753a567640954e952235ee
Anemone <tttaina@mail.ru>
2019-11-01 18:44:03 +0300

    Scripted auto-commit on change (2019-11-01 18:44:03) by gitwatch.sh

--- src/main/java/FlightsTableMapper.java
+++ src/main/java/FlightsTableMapper.java
@@ -1,18 +1,18 @@
-package sorryIdontKnowHowToNameIt;
+
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class FlightsTableMapper extends Mapper<LongWritable, Text, sorryIdontKnowHowToNameIt.AirportKey, Text> {
+public class FlightsTableMapper extends Mapper<LongWritable, Text, AirportKey, Text> {
     private static int CODE = 14, DELAY = 17;
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String codesAndAirports[] = sorryIdontKnowHowToNameIt.TablesParser.parseFlightsTable(value); //результат парсинга таблицы
         if (sorryIdontKnowHowToNameIt.TablesParser.checkFlight(codesAndAirports[DELAY])) {
             return;
         }
-        sorryIdontKnowHowToNameIt.AirportKey ak = new sorryIdontKnowHowToNameIt.AirportKey(sorryIdontKnowHowToNameIt.TablesParser.getID(codesAndAirports[CODE]),1);
+        AirportKey ak = new AirportKey(sorryIdontKnowHowToNameIt.TablesParser.getID(codesAndAirports[CODE]),1);
         Text delay = new Text(codesAndAirports[DELAY]);
         context.write(ak, delay);
     }
