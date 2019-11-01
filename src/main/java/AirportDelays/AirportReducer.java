f4cec1c241f2a70ee40573a70da18eff75c45543
Anemone <tttaina@mail.ru>
2019-11-01 19:59:12 +0300

    Scripted auto-commit on change (2019-11-01 19:59:12) by gitwatch.sh

--- src/main/java/AirportDelays/AirportReducer.java
+++ src/main/java/AirportDelays/AirportReducer.java
@@ -8,7 +8,10 @@ import java.util.Iterator;
 
 public class AirportReducer extends Reducer<AirportKey, Text, Text, Text> {
     public void reduce(AirportKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-
+        Iterator<Text> iter = values.iterator();
+        Text it = new Text(iter.next());
+        int count=0;
+        double maxTime=
         context.write(new Text(airportName), new Text("MIN time = "+minTime+" MAX time = "+maxTime+" Average time = "+averageTime));
     }
 }
