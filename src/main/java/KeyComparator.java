d80f95fd250c71ea82351adfa02d856644fda052
Anemone <tttaina@mail.ru>
2019-11-01 18:44:41 +0300

    Scripted auto-commit on change (2019-11-01 18:44:41) by gitwatch.sh

--- src/main/java/KeyComparator.java
+++ src/main/java/KeyComparator.java
@@ -1,4 +1,3 @@
-package sorryIdontKnowHowToNameIt;
 
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
@@ -6,12 +5,12 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class KeyComparator extends WritableComparator{
     protected KeyComparator() {
-        super(sorryIdontKnowHowToNameIt.AirportKey.class, true);
+        super(AirportKey.class, true);
     }
 
     public int compare(WritableComparable a, WritableComparable b) {
-        sorryIdontKnowHowToNameIt.AirportKey a1 = (sorryIdontKnowHowToNameIt.AirportKey)a;
-        sorryIdontKnowHowToNameIt.AirportKey b1 = (sorryIdontKnowHowToNameIt.AirportKey)b;
-        return sorryIdontKnowHowToNameIt.AirportKey.compare(a1.getAirportID(), b1.getAirportID());
+        AirportKey a1 = (AirportKey)a;
+        AirportKey b1 = (AirportKey)b;
+        return AirportKey.compare(a1.getAirportID(), b1.getAirportID());
     }
 }
