818e8d8110c135934eefe1e5175f3660d883a103
Anemone <tttaina@mail.ru>
2019-11-01 20:01:38 +0300

    Scripted auto-commit on change (2019-11-01 20:01:38) by gitwatch.sh

--- src/main/java/AirportDelays/AirportReducer.java
+++ src/main/java/AirportDelays/AirportReducer.java
@@ -3,6 +3,7 @@ package AirportDelays;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.awt.geom.Arc2D;
 import java.io.IOException;
 import java.util.Iterator;
 
@@ -11,7 +12,13 @@ public class AirportReducer extends Reducer<AirportKey, Text, Text, Text> {
         Iterator<Text> iter = values.iterator();
         Text it = new Text(iter.next());
         int count=0;
-        double maxTime=
+        double maxTime=0, minTime=Double.MAX_VALUE, averageTime=0, sum=0, delay;
+
+        while(iter.hasNext()) {
+            delay = Double.parseDouble(iter.next().toString());
+            
+        }
+
         context.write(new Text(airportName), new Text("MIN time = "+minTime+" MAX time = "+maxTime+" Average time = "+averageTime));
     }
 }
