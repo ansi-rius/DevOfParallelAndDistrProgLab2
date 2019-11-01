de900bf87b1e9c30280a712b4b85df29e571312a
Anemone <tttaina@mail.ru>
2019-11-01 18:43:41 +0300

    Scripted auto-commit on change (2019-11-01 18:43:41) by gitwatch.sh

--- src/main/java/AirportsTableMapper.java
+++ src/main/java/AirportsTableMapper.java
@@ -1,21 +1,21 @@
-package sorryIdontKnowHowToNameIt;
+
 
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import java.io.IOException;
 
-public class AirportsTableMapper extends Mapper<LongWritable, Text, sorryIdontKnowHowToNameIt.AirportKey, Text> {
+public class AirportsTableMapper extends Mapper<LongWritable, Text, AirportKey, Text> {
     private static int CODE = 0, DESCRIPTION = 1;
     @Override protected
     void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         //читать таблицы..
-        String codesAndAirports[] = sorryIdontKnowHowToNameIt.TablesParser.parseAirportsTable(value); //результат парсинга таблицы,
+        String codesAndAirports[] = TablesParser.parseAirportsTable(value); //результат парсинга таблицы,
         //работа с ключами..
         if (TablesParser.checkAir(codesAndAirports[CODE])) {
             return;
         }
-        sorryIdontKnowHowToNameIt.AirportKey ak = new sorryIdontKnowHowToNameIt.AirportKey(sorryIdontKnowHowToNameIt.TablesParser.getID(codesAndAirports[CODE]),0);
+        AirportKey ak = new AirportKey(TablesParser.getID(codesAndAirports[CODE]),0);
         Text des = new Text(codesAndAirports[DESCRIPTION]);
         context.write(ak, des);
     }
